fc1172a61b3ee9e437a0b7a07564e78097a6c8dd
L200170170 <48179896+L200170170@users.noreply.github.com>
2019-03-05 00:17:45 +0900

    Add files via upload

---
+++ Modul_1/Tugas2.py
@@ -0,0 +1,8 @@
+def persegiempat(x,y):
+    for i in range(x):
+        if i==0 or i== x-1:
+            print("@"*y)
+        else:
+            print("@"+" "*(y-2)+"@")
+
+persegiempat(3,5)
---
+++ Modul_1/Tugas4.py
@@ -0,0 +1,6 @@
+def rerata(b):
+    x=sum(b)/len(b)
+    print(x)
+
+x=[2,3,6,5]
+rerata(x)
